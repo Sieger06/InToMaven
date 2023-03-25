8cef2aaee33050d6e3856bbfb7b63f60e79a9ad9
Sieger06 <irek.haibulin@gmail.com>
2023-03-25 16:28:48 +0300

    Initial commit

---
+++ src/main/java/pro/sky/intomaven/InToMavenApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.intomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InToMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InToMavenApplication.class, args);
+    }
+
+}
